829568262e4385d19257ce24199ce189a43e7eca
Rahul Chauhan <rahulsp30101@gmail.com>
2020-05-24 16:03:15 +0530

    day 26

---
+++ day26_maxof_int.py
@@ -0,0 +1,11 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+#Submitted by thr3sh0ld
+def main():
+    x=input()
+    print(max(int(x) for x in str(x)),end='')
+
+ # Write code here 
+
+main()
+
